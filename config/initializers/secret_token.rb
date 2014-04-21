d9b698c3ee7dbd8c96754ca601b98e9983e067ef
Andy von Dohren <avondohren@gmail.com>
2014-04-21 15:09:46 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmacAngel::Application.config.secret_token = '5e82fe2532dbf5e9733c7b430318c4a511dc4f57fad34c038879199d6f000ea54b7f785b24c070678afa21304b0acf9d949e9d4b32b34cd3803e0e484cf9ce27'
