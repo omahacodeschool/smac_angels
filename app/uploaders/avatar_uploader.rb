d6b93382a05ed0fc33199735c9b2fd0c3a5a0185
locolan <lochlan.hehr@yahoo.com>
2014-04-22 15:18:53 -0500

    uploads working, make sure to update .env, run bundle, and run rake db:migrate !

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,7 +8,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # include CarrierWave::MimeTypes
   # process :set_content_type
-
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
