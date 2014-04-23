f6b22cdfcde0ae31bf65198685a399cdd32eed93
matthova <matthova87@gmail.com>
2014-04-22 22:49:10 -0500

    carrierwave update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials   = {
-    :provider              => ENV["CW_PROVIDER"],
-    :aws_access_key_id     => ENV["CW_ACCESS"],
-    :aws_secret_access_key => ENV["CW_SECRET"],
-    :region                => ENV["CW_REGION"]
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["CW_ACCESS"],             # required
+    :aws_secret_access_key  => ENV["CW_SECRET"],             # required
+    :region                 => ENV["CW_REGION"]              # optional
   }
-  config.fog_directory     = ENV["CW_DIR"]
+  config.fog_directory  = ENV["CW_DIR"]                      # required
 end
