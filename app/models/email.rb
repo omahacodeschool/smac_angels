c3970fa46d910d8bc441a2e68d573de96aef0fcc
matthova <matthova87@gmail.com>
2014-04-23 16:26:17 -0500

    email clean

--- app/models/email.rb
+++ app/models/email.rb
@@ -6,87 +6,46 @@ class Email < ActiveRecord::Base
   
   def email_new_user(user)
     template = Template.find_by_name("Signup Confirmation")
-
-    @email = email_init(template)
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+    send_email(template, user)
   end
 
-  def email_new_request(user)
-    @template = Template.find_by_name("Requestor Signup")
-
-    @email = Email.new
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+  def email_new_requestor(user)
+    template = Template.find_by_name("Requestor Signup")
+    send_email(template, user)
   end
 
-  def email_new_user(user)
-    @template = Template.find_by_name("Signup Confirmation")
-
-    @email = Email.new
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+  def email_new_angel(user)
+    template = Template.find_by_name("Angel Signup")
+    send_email(template, user)
   end
 
-  def email_new_user(user)
-    @template = Template.find_by_name("Signup Confirmation")
-
-    @email = Email.new
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+  def email_angel_shipping_info(user)
+    template = Template.find_by_name("Shipping Notification for Angel")
+    send_email(template, user)
   end
 
-  def email_new_user(user)
-    @template = Template.find_by_name("Signup Confirmation")
-
-    @email = Email.new
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+  def email_requestor_shipping_info(user)
+    template = Template.find_by_name("Shipping Notification for Requestor")
+    send_email(template, user)
   end
 
-  def email_new_user(user)
-    @template = Template.find_by_name("Signup Confirmation")
-
-    @email = Email.new
-    @email.content = @template.content
-    @email.subject = @template.name
-    @email.to_addresses = user.email
-
-    if @email.save
-      UserMailer.email_layout(@email).deliver    
-    end
+  def email_angel_feedback_notification(user)
+    template = Template.find_by_name("Feedback Notification for Angel")
+    send_email(template, user)
   end
 
-  def email_init(template)
+  def email_requestor_feedback_notification(user)
+    template = Template.find_by_name("Feedback Notification for Requestor")
+    send_email(template, user)
+  end
+
+  def send_email(template, user)
     email = Email.new
     email.content = template.content
     email.subject = template.name
     email.to_addresses = user.email
-    email
+    if email.save
+      UserMailer.email_layout(email).deliver    
+    end
   end
 end
