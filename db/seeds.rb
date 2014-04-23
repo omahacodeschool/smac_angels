bbb46bdeea50fa897daaba0d8fbec3969a88542d
Yofred Moik <ytmoik@gmail.com>
2014-04-23 13:12:01 -0400

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+User.create(fname: "Sarah", lname: "Jones", email: "sarah@example.com", password: "111", password_confirmation: "111")
+User.create(fname: "John", lname: "Doe", email: "john@example.com", password: "111", password_confirmation: "111")
+Request.create(requestor_id: 1, angel_id: 2, ship_address: "100 Main Street", ship_city: "Omaha", ship_state: "Nebraska", ship_zipcode: "10101", story: "Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.", external_url: "www.yahoo.com")
