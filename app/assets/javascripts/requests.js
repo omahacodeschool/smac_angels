99344d79655c9216c1894159a8fb18884a998abf
Johnathon <ja.lueth@gmail.com>
2014-04-23 15:22:32 -0500

    facebook comments on requests

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -7,40 +7,5 @@ $(document).ready(function() {
     else
         $('.obo_name').slideToggle('slow');
   });
-  
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : ENV['APP_ID'],
-      status     : true,
-      xfbml      : true
-    });
-    FB.ui(
-     {
-       method: 'feed',
-              name: 'SMAC Cancer',
-              caption: 'Uniting cancer victims and sock monkeys!',
-              description: (
-                 'Welcome'
-              ),
-              link: 'http://smacangels.com',
-              picture: ''
-             },
-             function(response) {
-               if (response && response.post_id) {
-                 alert('Post was published.');
-               } else {
-                 alert('Post was not published.');
-               }
-     }
-    ); 
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-  
+    
 });
