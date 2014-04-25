5d00f0b33061ac9ad07faa109f1bca569446b570
Todd Nichols <nivram80@gmail.com>
2014-04-25 10:41:46 -0500

    add signup section to login page and hide forgot password form until user clicks on Forgot Password

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,2 +1,8 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+	$('.forgot_pass_container').hide();
+	$('.forgot').click(function(){
+	    $('.forgot_pass_container').slideToggle('slow');
+	});
+
+});
