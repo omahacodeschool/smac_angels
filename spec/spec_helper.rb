e115a42e9370867e7f427859b05253ff7f0e4e19
Johnathon <ja.lueth@gmail.com>
2014-04-24 22:43:44 -0500

    a few tests, failing signup @ email, failing login at two email fields named the same

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,13 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-NegativeCaptcha.test_mode = true
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+  NegativeCaptcha.test_mode = true
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
