c80f58f568f4258e4a24fc00fee29a21a635a672
Andy von Dohren <avondohren@gmail.com>
2014-04-27 09:26:13 -0500

    homepage is working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 layout "homepage"
   
   def index
+    # @requests = Request.where("current_status = 0").sample(5)
+    @requests = Request.all.sample(5)
+    @filler = Request.all.sample(5 - @requests.length)
     
   end
 
