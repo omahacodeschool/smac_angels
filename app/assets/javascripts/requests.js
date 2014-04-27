3f6d6381b16a7d50816a7fab8d83d24d07967acf
Johnathon <ja.lueth@gmail.com>
2014-04-27 16:53:57 -0500

    remove old ajax query

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -21,17 +21,6 @@ $(document).ready(function() {
   });
 
 
-  // $(".rimage").hover(
-//     function () {
-//       originalText="<h2>It is a great feeling to be able to comfort someone in need. Please view the stories of those suffering with cancer who would like some creature comfort. Clicking a picture will take you to their profile.</h2>";
-//       var story = $(this).data("story");
-//       $("#story").hide().html(story).fadeIn('easing');
-//     }, 
-//     function () {
-//       $("#story").hide().html(originalText).fadeIn('easing');
-//     }
-//         );
-
   $(".img-req").hover(
     function () {
       debugger
