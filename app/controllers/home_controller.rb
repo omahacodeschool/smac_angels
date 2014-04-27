ffbf6d45048661a5e980c814ab38ef11172a3554
Andy von Dohren <avondohren@gmail.com>
2014-04-27 09:37:50 -0500

    changed queries for home page to match what we need in prod, not testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
 layout "homepage"
   
   def index
-    # @requests = Request.where("current_status = 0").sample(5)
-    @requests = Request.all.sample(5)
-    @filler = Request.all.sample(5 - @requests.length)
-    
+    @requests = Request.where("current_status = 0").sample(5)
+    @filler = Request.where("current_status > ? AND current_status < ?", 0, 11).sample(5 - @requests.length)
   end
 
 end
