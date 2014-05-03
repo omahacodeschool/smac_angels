2b4515b8b49c5ba7abfb1375206cf0d29002609f
matthova <matthova87@gmail.com>
2014-05-03 08:08:52 -0500

    form submit working

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-  
+
 	if($("#nominate").data("nominate")===true){
 	  $('.obo_name').hide();
     $('.obo_name').slideToggle('slow').trigger('autosize.resize');
@@ -10,7 +10,7 @@ $(document).ready(function() {
     $('.obo_name').hide();
     $('.obo_check input:checkbox').removeAttr('checked')
   };
-  
+
   $('.obo_check').change(function(){
     if(this.checked) {
         $('.obo_name').slideToggle('slow').trigger('autosize.resize');
@@ -30,28 +30,33 @@ $(document).ready(function() {
     cyclic: true
   });
 
-  
-  
+
+
 
   $(".img-req").hover(
     function () {
       originalText="<h2>It is a great feeling to be able to comfort someone in need. Please view the stories of those battling cancer who would like some creature comfort by scrolling over their name. Clicking a picture will take you to their profile.</h2>";
       var story = $(this).data("story");
       $("#story").hide().html(story).fadeIn('easing');
-    }, 
+    },
     function () {
       $("#story").hide().html(originalText).fadeIn('easing');
     }
   );
-  
-  if(".upload_photo"){
+
+  if($(".upload_photo").length){
     $(".upload_photo").click(function(){
       $('.upload_after_photo').trigger('click');
     });
   }
-  
-  document.getElementById("").onchange = function() {
-      document.getElementById("form").submit();
-  };
-  
+
+  //Rather than using getElementByID and .erb
+  //I am assuming there will be only one form on this page
+  if($("#request_after_photo_url").length){
+    document.getElementById('request_after_photo_url').onchange = function(){
+      var form = document.getElementsByClassName("edit_request")[0];
+      form.submit();
+    };
+  }
+
 });
