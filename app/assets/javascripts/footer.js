6161242291a17a111280d7150fd3ca3baf417a7e
matthova <matthova87@gmail.com>
2014-05-05 13:25:35 -0500

    initi commit

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){      
-      
+$(document).ready(function(){
+
     $('.hover').hover(function(){
         $(this).addClass('flip');
     },function(){
         $(this).removeClass('flip');
     });
-    
+
 		$('a.top').click(function() {
       $('html, body').animate({scrollTop:0}, 'slow');
 			return false;
     });
-    
+  
 });
